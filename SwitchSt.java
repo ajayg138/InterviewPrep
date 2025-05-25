c7ce3a3e482709106ea36a435248cc8df036d9bc
ajayg138 <ajaygole728@gmail.com>
2025-05-25 18:44:01 +0530

    Switch Statement Eg...

---
+++ SwitchSt.java
@@ -0,0 +1,32 @@
+public class SwitchSt {
+
+    public static void switchEg(int x){
+
+        switch (x){
+            case 1:
+                System.out.println("switch 1...");
+                break;
+            case 2:
+                System.out.println("switch 2...");
+                break;
+            case 3:
+                System.out.println("switch 3...");
+                break;
+            case 4:
+                System.out.println("switch 4...");
+                break;
+            case 5:
+                System.out.println("switch 5...");
+                break;
+            case 6:
+                System.out.println("switch 6...");
+                break;
+            default:
+                System.out.println("Invalid Input...");
+        }
+    }
+
+    public static void main(String[] args) {
+    switchEg(99);
+    }
+}
--- vararg.java
+++ vararg.java
@@ -3,10 +3,12 @@ public class vararg {
         System.out.println("Hello Ajay...");
     }
 
-    public static void main(String[] args) {
+//   final synchronized strictfp public static void main(String[] args) {
+       public static void main(String[] args) {
         m1();
         m1(3);
         m1(10);
-        m1(18);
+        m1(18,12,12);
+           System.out.println('a'+'b');
     }
 }
