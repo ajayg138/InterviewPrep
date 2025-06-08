aeb537dc2e4e4ec091f0fa567ce3f0d14ecef7ab
ajayg138 <ajaygole728@gmail.com>
2025-06-08 23:24:44 +0530

    pattern completed...

--- Patterns.java
+++ Patterns.java
@@ -543,10 +543,10 @@ public class Patterns {
 
         public static void pattern22(int n){
 
-        for(int i=0;i<2*n;i++){
+        for(int i=0;i<n;i++){
 
             //stars
-            for(int j=0;j<n-i+1;j++){
+            for(int j=0;j<n-i;j++){
                 System.out.print("*");
             }
 
@@ -559,7 +559,7 @@ public class Patterns {
 
 
             //stars
-            for(int j=n-i+1;j>=1;j--){
+            for(int j=0;j<n-i;j++){
                 System.out.print("*");
             }
 
@@ -568,9 +568,95 @@ public class Patterns {
 
 
         }
+
+        for(int i=0;i<n;i++){
+            //stars
+            for(int j=0;j<=i;j++){
+                System.out.print("*");
+            }
+
+            //spaces
+            for(int j=0;j<2*(n-i-1);j++){
+                System.out.print(" ");
+            }
+
+            //stars
+            for(int j=0;j<=i;j++){
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
         }
 
+        public static void pattern23(int n){
+        int space = 2*n-2;
+            for(int i=1;i<=2*n-1;i++){
+                int stars=i;
+                if(i>n) stars=2*n-i;
 
+                //stars
+                for(int j=1;j<=stars;j++){
+                    System.out.print("*");
+                }
+
+                //spaces
+                for(int j=1;j<=space;j++){
+                    System.out.print(" ");
+                }
+
+
+                //stars
+                for(int j=1;j<=stars;j++){
+                    System.out.print("*");
+                }
+                System.out.println();
+                if(i<n) space -= 2;
+                else space += 2;
+            }
+        }
+
+        public static void pattern24(int n){
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 || j==0 || i==n-1 || j==n-1){
+                    System.out.print("*");
+                }else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+        }
+
+//        public static void pattern25(int n){
+//        for(int i=0;i<2*n-1;i++){
+//            for(int j=0;j<2*n-1;j++){
+//                int top=i;
+//                int left=j;
+//                int right=(2*n-2)-j;
+//                int down=(2*n-2)-i;
+//                System.out.print(n-min(min(top,down)), min(left,right));
+//            }
+//            System.out.println();
+//        }
+//        }
+
+    public static void pattern25(int n) {
+        int size = 2 * n - 1;
+        for(int i = 0; i < size; i++) {
+            for(int j = 0; j < size; j++) {
+                int top = i;
+                int left = j;
+                int right = size - 1 - j;
+                int down = size - 1 - i;
+
+                int minDistance = Math.min(Math.min(top, down), Math.min(left, right));
+                System.out.print(n - minDistance);
+            }
+            System.out.println();
+        }
+    }
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.print("Enter n: ");
@@ -613,6 +699,9 @@ public class Patterns {
 //        pattern19(n);
 //        pattern20(n);
 //        pattern21(n);
-        pattern22(n);
+//        pattern22(n);
+//        pattern23(n);
+//        pattern24(n);
+        pattern25(n);
     }
 }
