98b053dbaa5ee64425413608ec6ea9b88290c6a1
ajayg138 <ajaygole728@gmail.com>
2025-06-10 10:15:29 +0530

    GCD Optimal...

--- GCD.java
+++ GCD.java
@@ -14,15 +14,32 @@ public class GCD {
 //        return GCD;
 //    }
 
-    public static int greatestCommonDivisor(int n1, int n2){
-        int GCD=1;
-        for(int i=Math.min(n1,n2);i>=1;i--){
-            if(n1%i==0 && n2%i==0){
-                GCD=i;
-                break;
+    //Brute Force
+//    public static int greatestCommonDivisor(int n1, int n2){
+//        int GCD=1;
+//        for(int i=Math.min(n1,n2);i>=1;i--){
+//            if(n1%i==0 && n2%i==0){
+//                GCD=i;
+//                break;
+//            }
+//        }
+//        return GCD;
+//    }
+
+    public static int greatestCommonDivisor(int n1,int n2){
+        while(n1>0 && n2>0){
+            if(n1>n2){
+                n1=n1%n2;
+            }else {
+                n2=n2%n1;
             }
         }
-        return GCD;
+
+        if(n1==0){
+            return n2;
+        }else {
+            return n1;
+        }
     }
 
     public static void main(String[] args) {
