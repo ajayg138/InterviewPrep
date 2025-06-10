e5a5e41f5f57eca576034520f5b51f80d3341e5c
ajayg138 <ajaygole728@gmail.com>
2025-06-10 09:38:21 +0530

    Print Divisor-Optimize...

--- AllDivisor.java
+++ AllDivisor.java
@@ -1,14 +1,39 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
 
 public class AllDivisor {
+//
+//    //BruteForce
+//    public static void divisor(int n){
+//        for(int i=1;i<=n;i++){
+//            if(n%i==0){
+//                System.out.println(i);
+//            }
+//        }
+//    }
 
+
+    //Optimal
     public static void divisor(int n){
-        for(int i=1;i<=n;i++){
+        List<Integer> div=new ArrayList<>();
+
+        for(int i=1;i<=Math.sqrt(n);i++){
             if(n%i==0){
-                System.out.println(i);
+                div.add(i);
+                if(n/i != i){
+                    div.add(n/i);
+                }
             }
         }
+
+        Collections.sort(div);
+        for(int d:div){
+            System.out.println(d);
+        }
     }
+
 
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
