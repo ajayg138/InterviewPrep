36cf7d5689d880a928e12535c9a9a08e49f69e84
ajayg138 <ajaygole728@gmail.com>
2025-06-10 22:44:48 +0530

    Recursion Problems...

--- RecursionProblems.java
+++ RecursionProblems.java
@@ -50,6 +50,15 @@ public class RecursionProblems {
 //        printNum(1,n);
 //        printNumRev(n);
 //        printNumBackTracking(n,n);
-        printNumBackTrackingRev(1,n);
+//        printNumBackTrackingRev(1,n);
+
+        //checking
+//        String s1=new String("Durga");
+//        String s2=new String("Software");
+//        String s3=s1;
+//        System.out.println(s1==s2);
+//        System.out.println(s1.equals(s2));
+//        System.out.println(s1.equals(s3));
+
     }
 }
