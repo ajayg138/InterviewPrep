0f4f9c17a9103f3056927f0f80fe323c520458f3
ajayg138 <ajaygole728@gmail.com>
2025-06-13 23:33:07 +0530

    Anagram...

--- Anagram.java
+++ Anagram.java
@@ -1,19 +1,43 @@
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Anagram {
 
     //brute Force
-    public boolean checkAnagram(String str1, String str2){
-        if(str1.length() != str2.length()){
+//    public boolean checkAnagram(String str1, String str2){
+//        if(str1.length() != str2.length()){
+//            return false;
+//        }
+//
+//        char[] arr1=str1.toCharArray();
+//        char[] arr2=str2.toCharArray();
+//
+//        return Arrays.equals(arr1,arr2);
+//    }
+
+
+
+    //Optimal Approach
+    public boolean checkAnagram(String s, String t) {
+        if(s.length() != t.length()){
             return false;
         }
 
-        char[] arr1=str1.toCharArray();
-        char[] arr2=str2.toCharArray();
+        HashMap<Character, Integer> map1=new HashMap<>();
+        HashMap<Character, Integer> map2=new HashMap<>();
 
-        return Arrays.equals(arr1,arr2);
+        for(int i=0;i<s.length();i++){
+            char ch1=s.charAt(i);
+            char ch2=s.charAt(i);
+
+
+            map1.put(ch1,map1.getOrDefault(ch1,0)+1);
+            map2.put(ch2, map2.getOrDefault(ch2, 0)+1);
+        }
+        return map1.equals(map2);
     }
+
 
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
