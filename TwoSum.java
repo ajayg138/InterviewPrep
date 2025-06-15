e23ec15999eab4f421eb2c998ebf32ca595d334a
ajayg138 <ajaygole728@gmail.com>
2025-06-15 15:14:10 +0530

    TwoSum Optimize...

--- TwoSum.java
+++ TwoSum.java
@@ -1,20 +1,36 @@
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class TwoSum {
 
-    public static int[] giveSum(int[] arr, int target){
-        int[] ans=new int[2];
+//    public static int[] giveSum(int[] arr, int target){
+//        int[] ans=new int[2];
+//
+//        for(int i=0;i<arr.length;i++){
+//            for(int j=i+1;j<arr.length;j++){
+//                if(arr[i]+arr[j]==target){
+//                    ans[0]=i;
+//                    ans[1]=j;
+//                    return ans;
+//                }
+//            }
+//        }
+//        return ans;
+//    }
+
+    //optimize solution
+    public static int[] giveSum(int[] arr,int target){
+        HashMap<Integer, Integer> map = new HashMap<>();
 
         for(int i=0;i<arr.length;i++){
-            for(int j=i+1;j<arr.length;j++){
-                if(arr[i]+arr[j]==target){
-                    ans[0]=i;
-                    ans[1]=j;
-                    return ans;
-                }
+            int complement=target - arr[i];
+
+            if(map.containsKey(complement)){
+                return new int[]{map.get(complement),i};
             }
+            map.put(arr[i],i);
         }
-        return ans;
+        return new int[] {-1,-1};
     }
 
     public static void main(String[] args) {
@@ -31,9 +47,17 @@ public class TwoSum {
         System.out.print("Enter Target: ");
         int target=sc.nextInt();
 
-        int[] sum=giveSum(arr,target);
-        for(int i=0;i<sum.length;i++){
-            System.out.print(sum[i]+" ");
+//        int[] sum=giveSum(arr,target);
+//        for(int i=0;i<sum.length;i++){
+//            System.out.print(sum[i]+" ");
+//        }
+
+        int result[]=giveSum(arr,target);
+
+        if(result[0] == -1){
+            System.out.println("No two sum solution found.");
+        }else{
+            System.out.println("Indices: "+result[0]+" "+result[1]);
         }
 
     }
