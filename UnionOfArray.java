68341503a050a160bcd7fe4f9e67964c3faa6683
ajayg138 <ajaygole728@gmail.com>
2025-06-18 21:23:34 +0530

    ArrayList...

--- UnionOfArray.java
+++ UnionOfArray.java
@@ -1,7 +1,8 @@
 import java.util.ArrayList;
+import java.util.HashSet;
 
 public class UnionOfArray {
-    public static ArrayList<Integer> union(int[] arr1,int[] arr2){
+    public static ArrayList<Integer> unionPrint(int[] arr1,int[] arr2){
         ArrayList<Integer> List=new ArrayList<>();
         int n1=arr1.length;
         int n2=arr2.length;
@@ -46,11 +47,44 @@ public class UnionOfArray {
     }
 
 
+
+    public static int union(int[] arr1, int[] arr2) {
+        HashSet<Integer> set = new HashSet<>();
+
+        for(int i=0;i<arr1.length;i++){
+            set.add(arr1[i]);
+        }
+
+        for(int j=0;j<arr2.length;j++){
+            set.add(arr2[j]);
+        }
+
+
+        return set.size();
+    }
+
+    public static HashSet<Integer> giveUnion(int[] arr1, int[] arr2) {
+        HashSet<Integer> set = new HashSet<>();
+
+        for(int i=0;i<arr1.length;i++){
+            set.add(arr1[i]);
+        }
+
+        for(int j=0;j<arr2.length;j++){
+            set.add(arr2[j]);
+        }
+
+
+        return set;
+    }
+
     public static void main(String[] args) {
         int arr1[] = {7,3,9};
         int arr2[] = {6,3,9,2,9,4};
 
-        System.out.println(union(arr1,arr2));
+//        System.out.println(union(arr1,arr2));
+//        System.out.println(union(arr1,arr2));
+        System.out.println(giveUnion(arr1,arr2));
 
     }
 }
