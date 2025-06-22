279238de084d75ff8915471351b58421b2040d4c
ajayg138 <ajaygole728@gmail.com>
2025-06-23 00:55:17 +0530

    Remove Dup

--- PrintDup.java
+++ PrintDup.java
@@ -1,6 +1,9 @@
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class PrintDup {
+    //Brute Force
     public static ArrayList<Integer> giveDuplicates(int[] arr){
         ArrayList<Integer> ans=new ArrayList<>();
 
@@ -16,10 +19,29 @@ public class PrintDup {
         return ans;
     }
 
+    //Optimal
+    public static ArrayList<Integer> returnDuplicates(int[] arr){
+        HashMap<Integer,Integer> map=new HashMap<>();
+        ArrayList<Integer> res=new ArrayList<>();
+
+        for(int i=0;i<arr.length;i++){
+            map.put(arr[i], map.getOrDefault(arr[i],0)+1);
+        }
+
+        for(Map.Entry<Integer,Integer> ans:map.entrySet()){
+            if(ans.getValue() != 1){
+                res.add((ans.getKey()));
+            }
+        }
+        return res;
+    }
+
     public static void main(String[] args) {
         int[] arr={1,5,4,2,1,4,7,7};
 
         System.out.print(giveDuplicates(arr));
+        System.out.println();
+        System.out.print(returnDuplicates(arr));;
 
     }
 }
