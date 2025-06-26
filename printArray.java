357b7cc8bcb154e422a5846586cf011aade96c69
ajayg138 <ajaygole728@gmail.com>
2025-06-27 00:01:14 +0530

    Print array...

---
+++ printArray.java
@@ -0,0 +1,9 @@
+public class printArray {
+    public static void main(String[] args) {
+        int[] arr={1,2,3,4,4,5,5,5};
+
+        for(int num: arr){
+            System.out.println(num);
+        }
+    }
+}
